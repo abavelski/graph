95825ea603ed6d7e2ee5a1be72a81a1ec2f6f344
Alexei Bavelski <alexei.bavelski@nordea.com>
2016-04-25 15:23:07 +0200

    removed unused code

--- src/app/constants.js
+++ src/app/constants.js
@@ -1,5 +1,2 @@
 
-export const FETCH_DATA = 'FETCH_DATA'
-export const INCREASE = 'INCREASE'
-export const DECREASE = 'DECREASE'
-export const TOGGLE= 'TOGGLE'
+export const FETCH_DATA = 'FETCH_DATA';
--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,3 +1,1 @@
-export count from './count'
-export widget from './widget'
 export fetching from './fetching'
